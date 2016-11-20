c2e76a34e01275268a0be19485ba985efed94691
Tom Miller <tmiller@eab.com>
2016-11-19 21:18:46 -0600

    003.3 fix bug with identifier not being set

--- todo/Main.elm
+++ todo/Main.elm
@@ -68,7 +68,7 @@ update msg model =
         Add ->
             { model
                 | todos = model.todo :: model.todos
-                , todo = newTodo
+                , todo = { newTodo | identifier = model.nextIdentifier }
                 , nextIdentifier = model.nextIdentifier + 1
             }
 
