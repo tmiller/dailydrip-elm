8551e148961c0b959d3a1bea9c35b1a09922d16d
Tom Miller <tmiller@eab.com>
2016-11-27 08:31:56 -0600

    004.4: create new counter app

---
+++ counter2/Main.elm
@@ -0,0 +1,65 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Html.App as App
+
+
+type alias Model =
+    { count : Int
+    , increment : Int
+    , decrement : Int
+    }
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+initialModel : Model
+initialModel =
+    { count = 0
+    , increment = 0
+    , decrement = 0
+    }
+
+
+increment : Model -> Model
+increment model =
+    { model | count = model.count + 1, increment = model.increment + 1 }
+
+
+decrement : Model -> Model
+decrement model =
+    { model | count = model.count - 1, decrement = model.decrement + 1 }
+
+
+update : Msg -> Model -> Model
+update msg =
+    case msg of
+        Increment ->
+            increment
+
+        Decrement ->
+            decrement
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Increment ] [ text "+" ]
+        , div [] [ text (toString model.count) ]
+        , button [ onClick Decrement ] [ text "-" ]
+        , h3 [] [ text ("+ clicked " ++ (toString model.increment) ++ " times") ]
+        , h3 [] [ text ("- clicked " ++ (toString model.decrement) ++ " times") ]
+        ]
+
+
+main : Program Never Model Msg
+main =
+    App.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
